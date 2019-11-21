155a87efc4ca91c009a739e9686ebbf3a845e789
Laura Klions <ljklions@gmail.com>
2019-11-21 15:29:35 -0500

    updates

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
-CFLAGS = -std=c++11 -Wall
+CFLAGS = -std=c++11 -Wall 
 CC = g++
 
 all: build
 
 build: collegemain.o course.o college.o
-	$(CC) $(CFLAGS) collegemain.o course.o college.o -o all
+		$(CC) $(CFLAGS) collegemain.o course.o college.o -o all
 
-collegemain.o: collegemain.o course.o college.o
-	$(CC) $(CFLAGS) collegemain.o course.o college.o -o all
+collegemain: collegemain.o course.o college.o
+		$(CC) $(CFLAGS) collegemain.o course.o college.o -o all
 
 course.o: course.cc course.h
 	$(CC) $(CFLAGS) course.cc
@@ -24,11 +24,10 @@ runtest: test_college
 	./test_college
 
 test_college.o: test_college.cc
-		$(CC) $(CFLAGS) test_college.cc
+		$(CC) $(CFLAGS) test_college.cc -c
 
 test_college: college.o test_college.o course.o
 	$(CC) -o test_college test_college.o college.o course.o
-
 
 clean:
 	rm -f *.o collegemain test_college
--- test_college.cc
+++ test_college.cc
@@ -4,86 +4,43 @@
 #include "course.h"
 #include "node.h"
 
-
-//Test gpa() methos in the College class
-/*
-double College::gpa(){
-	node * ptr;
-	ptr = head;
-	double sum = 0;
-	while(ptr != NULL){
-	sum = sum + (ptr->data().get_number_grade() * ptr->data().get_hours());
-	ptr = ptr->link();
-	}
-	return (sum / hours());
-}
-*/
-
-/*
-double course::get_number_grade()const{
-    if(grade == "A") return 4.0;
-    if(grade == "A-") return 3.667;
-    if(grade == "B+") return 3.333;
-    if(grade == "B") return 3.0;
-    if(grade == "B-") return 2.667;
-    if(grade == "C+") return 2.333;
-    if(grade == "C") return 2.0;
-    if(grade == "C-") return 1.667;
-    if(grade == "D+") return 1.333;
-    if(grade == "D") return 1.0;
-    if(grade == "D-") return 0.667;
-    if(grade == "F") return 0.0;
-    else return 0;
-}
-*/
-
-/*
-void set_course(std::string num, std::string grad, double hrs);
-*/
-TEST_CASE( "All A's Should return 4.0" ) {
-	College ohio("Laura");
-	course a,b,c;
-	a.set_course("cs2401","a",3);
-	b.set_course("cs2400","a",4);
-	c.set_course("ise3200","a",2);
-
-	ohio.add(a);
-	ohio.add(b);
-	ohio.add(c);
-
-	double result = ohio.gpa();
-
-	REQUIRE(result == 4.0);
- }
-
 TEST_CASE( "A, B, and C with same weight should 3.0" ) {
-	College ohio("Laura");
+	College Kevin("Kevin");
 	course a,b,c;
- 	a.set_course("cs2401","a",3);
- 	b.set_course("cs2400","b",3);
- 	c.set_course("ise3200","c",3);
+ 	a.set_course("cs2401","A",3);
+	Kevin.add(a);
+ 	b.set_course("cs2400","B",3);
+	Kevin.add(b);
+ 	c.set_course("ise3200","C",3);
+	Kevin.add(c);
 
- 	ohio.add(a);
- 	ohio.add(b);
- 	ohio.add(c);
-
-	double result = ohio.gpa();
-
- 	REQUIRE(result == 3.0);
+ 	REQUIRE(Kevin.gpa() == 3.0);
 }
 
-TEST_CASE( "All D's should be 1.0" ) {
-	College ohio("Laura");
-	course a,b,c;
- 	a.set_course("cs2401","d",7);
- 	b.set_course("cs2400","d",4);
- 	c.set_course("ise3200","d",2);
 
- 	ohio.add(a);
- 	ohio.add(b);
- 	ohio.add(c);
+TEST_CASE( "All A's should return 4.0" ) {
+	College Carolyn("Carolyn");
+	course d,e,f;
+ 	d.set_course("ee1024","A",6);
+	Carolyn.add(d);
+	e.set_course("cs3610","A",3);
+	Carolyn.add(e);
+	f.set_course("ET2200","A",2);
+	Carolyn.add(f);
 
-	double result = ohio.gpa();
+ 	REQUIRE(Carolyn.gpa() == 4.0);
+}
 
- 	REQUIRE(result == 1.0);
+
+TEST_CASE( "All D's should return 1.0" ) {
+	College Julie("Julie");
+	course h,i,j;
+ 	h.set_course("cs200","D",1);
+	Julie.add(h);
+	i.set_course("ma3200","D",3);
+	Julie.add(i);
+	j.set_course("cs2401","D",9);
+	Julie.add(j);
+
+ 	REQUIRE(Julie.gpa() == 1.0);
 }
